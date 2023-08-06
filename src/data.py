602b3f93e93d7d8dc52aa26c59038f4b59be5958
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2023-08-06 17:43:43 +0300

    add data, mip solver and validation

---
+++ src/data.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+def read_fatahi_dataset(path_to_file):
+    """
+    Fatahi Valilai, Omid. “Dataset for Logistics and Manufacturing 
+    Service Composition”. 17 Mar. 2021. Web. 9 June 2023.
+    """
+    sheet_names = [
+        '5,10,10-1',
+        '5,10,10-2',
+        '5,10,10-3',
+        '10,10,10-1',
+        '10,10,10-2',
+        '10,10,10-3',
+        '5,10,20-1',
+        '5,10,20-2',
+        '5,10,20-3',
+        '5,20,10-1',
+        '5,20,10-2',
+        '5,20,10-3',
+        '5,20,20-1',
+        '5,20,20-2',
+        '5,20,20-3',
+        '5,5,5-1',
+        '5,5,5-2',
+        '5,5,5-3',
+    ]
+    res = []
+    for sheet_name in tqdm(sheet_names):
+        res.append(_read_sheet(path_to_file, sheet_name))
+    return res
+
+
+def _read_sheet(path_to_file, sheet_name):
+    n_services = 1
+    n_tasks, n_operations, n_cities, _ = list(
+            map(int, '-'.join(sheet_name.split(',')).split('-'))
+        )
+    operation = np.zeros((n_operations, n_tasks))
+    dist = np.zeros((n_cities, n_cities))
+    time_cost = np.zeros((n_operations, n_cities))
+    op_cost = np.zeros((n_operations, n_cities))
+    productivity = np.zeros((n_cities))
+    transportation_cost = np.zeros((n_services))
+
+    operation[:, :] = pd.read_excel(
+        path_to_file, 
+        sheet_name=sheet_name, 
+        header=None, 
+        usecols=range(1, n_tasks+1),
+        skiprows=5,
+        nrows=n_operations,
+    )
+    dist[:, :] = pd.read_excel(
+        path_to_file, 
+        sheet_name=sheet_name, 
+        header=None, 
+        usecols=range(1, n_cities+1), 
+        skiprows=5*2+n_operations-1,
+        nrows=n_cities,
+    )
+    time_cost[:, :] = pd.read_excel(
+        path_to_file, 
+        sheet_name=sheet_name, 
+        header=None, 
+        usecols=range(1, n_cities+1), 
+        skiprows=5*3+n_operations+n_cities-1*2,
+        nrows=n_operations,
+    )
+    time_cost[np.isinf(time_cost)] = 99
+    op_cost[:, :] = pd.read_excel(
+        path_to_file, 
+        sheet_name=sheet_name, 
+        header=None, 
+        usecols=range(1, n_cities+1), 
+        skiprows=5*4+n_operations+n_cities+n_operations-1*3,
+        nrows=n_operations,
+    )
+    op_cost[np.isinf(op_cost)] = 999
+    productivity[:] = pd.read_excel(
+        path_to_file, 
+        sheet_name=sheet_name, 
+        header=None, 
+        usecols=range(n_cities), 
+        skiprows=5*5+n_operations+n_cities+n_operations+n_operations-1*4,
+        nrows=1,
+    )
+    transportation_cost[:] = [0.3]
+    return {
+        'name': sheet_name,
+        'n_tasks': n_tasks, 
+        'n_operations': n_operations, 
+        'n_cities': n_cities,
+        'n_services': n_services,
+        'operation': operation,
+        'dist': dist,
+        'time_cost': time_cost,
+        'op_cost': op_cost,
+        'productivity': productivity,
+        'transportation_cost': transportation_cost,
+    }
+
---
+++ src/mip_solver.py
@@ -0,0 +1,40 @@
+import numpy as np
+from mip import Model, BINARY, MINIMIZE, xsum
+from itertools import product
+
+def mip_solve(
+        n_tasks, 
+        n_operations, 
+        n_cities, 
+        n_services, 
+        operation, 
+        dist, 
+        time_cost, 
+        op_cost, 
+        productivity, 
+        transportation_cost
+    ):
+    
+    gamma_shape = (n_operations, n_tasks, n_cities)
+    delta_shape = (n_services, n_cities, n_cities, n_operations - 1, n_tasks)
+
+    model = Model(sense=MINIMIZE)
+    gamma = model.add_var_tensor(shape=gamma_shape, name="gamma", var_type=BINARY)
+    delta = model.add_var_tensor(shape=delta_shape, name="delta", var_type=BINARY)
+    for i, k in product(range(n_operations), range(n_tasks)):
+        model += (xsum(gamma[i, k]) == operation[i, k])
+    for i, k, m, m_ in product(
+        range(n_operations-1), range(n_tasks), range(n_cities), range(n_cities)):
+        seq = np.where(operation[i:, k] == 1)[0]
+        if operation[i, k] and len(seq) > 1:
+            model += (gamma[i, k, m] + gamma[i+seq[1], k, m_] - 1 <= xsum(delta[:, m, m_, i, k]))
+    total_op_cost = np.sum((time_cost * op_cost / productivity[None, :])[:, None, :] * gamma)
+    total_logistic_cost = np.sum((transportation_cost[:, None, None] * dist[None, ...])[..., None, None] * delta)
+    model.objective = total_op_cost + total_logistic_cost
+
+    status = model.optimize(max_seconds=120)
+    
+    _delta = np.reshape(list(map(lambda x: x.x, delta.flatten().tolist())), delta_shape)
+    _gamma = np.reshape(list(map(lambda x: x.x, gamma.flatten().tolist())), gamma_shape)
+    
+    return _delta, _gamma, status, model.objective_value
