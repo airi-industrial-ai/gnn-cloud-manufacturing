f57deef8c2c3296f39eaeba3527327c09f44d8cb
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-11-07 18:14:46 +0100

    update naive solution

--- cloudmanufacturing/solvers/naive_solver.py
+++ cloudmanufacturing/solvers/naive_solver.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-class naive_solver():
+
+class naive_solver:
     def __init__(self, dataset):
         self.problems = dataset
-    
+        self.total_cost = 0
 
-    def solve_sub_problem(self, available_operations, cost_operations, trans_cost):
+    def solve_suboperaion(self, available_operations, cost_operations, trans_cost):
+        """
+        Solve problem for one suboperation
+        """
         sub_problem_data = []
         for i, stage in enumerate(available_operations):
             if i == 0:
@@ -14,35 +18,49 @@ class naive_solver():
             else:
                 cost_total = cost_operations[stage] + trans_cost[city]
                 city = np.argmin(cost_total)
-                sub_problem_data.append([city, cost_operations[stage][city]])        
-        return np.array(sub_problem_data)[:,0], np.sum(np.array(sub_problem_data)[:,1])
+                sub_problem_data.append([city, cost_operations[stage][city]])
+        return np.array(sub_problem_data)[:, 0], np.sum(
+            np.array(sub_problem_data)[:, 1]
+        )
 
-    
-
-    def solve_problem(self, num_problem):
-        # Extract data
-        n_tasks = self.problems[num_problem]['n_tasks']
-        n_operations = self.problems[num_problem]['n_operations']
-        n_cities = self.problems[num_problem]['n_cities']
-        n_services = self.problems[num_problem]['n_services']
-        operation = self.problems[num_problem]['operation']
-        dist = self.problems[num_problem]['dist']
-        time_cost = self.problems[num_problem]['time_cost']
-        op_cost = self.problems[num_problem]['op_cost']
-        productivity = self.problems[num_problem]['productivity']
-        transportation_cost = self.problems[num_problem]['transportation_cost']
+    def solve_problem(self, num_problem, save_costs=False):
+        """
+        Solve operation
+        """
+        n_tasks = self.problems[num_problem]["n_tasks"]
+        operation = self.problems[num_problem]["operation"]
+        dist = self.problems[num_problem]["dist"]
+        time_cost = self.problems[num_problem]["time_cost"]
+        op_cost = self.problems[num_problem]["op_cost"]
+        productivity = self.problems[num_problem]["productivity"]
+        transportation_cost = self.problems[num_problem]["transportation_cost"]
 
         # Create cost matrices
         cost_operations = time_cost * op_cost / productivity
         trans_cost = dist * transportation_cost
 
-        total_cost = 0
-        total_path = {}
-        for task_number in range(n_tasks):
-            available_operations = np.nonzero(operation[:,task_number])[0]
-            path, cost = self.solve_sub_problem(available_operations, cost_operations, trans_cost)
-            
-            total_cost += cost
-            total_path[f'subproblem_{task_number}'] = path
-        
-        return total_path, total_cost
+        problem_cost = 0
+        problem_path = {}
+        for n_sub in range(n_tasks):
+            available_operations = np.nonzero(operation[:, n_sub])[0]
+            path, cost = self.solve_suboperaion(
+                available_operations, cost_operations, trans_cost
+            )
+
+            problem_cost += cost
+            problem_path[f"suboperation_{n_sub}"] = path
+        if save_costs:
+            self.total_cost += problem_cost
+        return {"path": problem_path, "cost": problem_cost}
+
+    def solve_all(self):
+        self.total_cost = 0
+        problem_info = {
+            f"problem_{num}": {
+                "path": self.solve_problem(num, save_costs=True)["path"],
+                "cost": self.solve_problem(num, save_costs=True)["cost"],
+            }
+            for num in range(len(self.problems))
+        }
+
+        return problem_info
