11d92718d984fdbe33e5600f63920e8011bb4e5d
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-11-13 22:59:12 +0100

    create naive solver++

--- cloudmanufacturing/solvers/naive_solver++.py
+++ cloudmanufacturing/solvers/naive_solver++.py
@@ -2,25 +2,47 @@ import numpy as np
 
 
 class NaiveSolver:
-    def __init__(self, dataset):
+    def __init__(self, dataset, n_bests=4, step_forward=3):
         self.problems = dataset
         self.total_cost = 0
         self.city = None
+        self.n_best = n_bests
+        self.step_forward = step_forward
+        self.cost_operations = None
+        self.trans_cost = None
+
+    def look_forward(self, available_operations, city=None):
+        city_list = []
+        cost = 0
+        for stage in available_operations:
+            if city is None:
+                city = np.random.choice(
+                    np.argsort(self.cost_operations[stage])[: self.n_best]
+                )
+                cost += self.cost_operations[stage][city]
+            else:
+                cost_total = self.cost_operations[stage] + self.trans_cost[city]
+                city = np.random.choice(np.argsort(cost_total)[: self.n_best])
+                cost += cost_total[city]
+
+            if len(available_operations[1:]) > 0:
+                cost, city = self.look_forward(available_operations[1:], city)
+            city_list.append(city)
+            return cost, city
 
     def solve_suboperaion(self, available_operations, cost_operations, trans_cost):
         """
         Solve problem for one suboperation
         """
         sub_problem_data = []
-        city = None
         for stage in available_operations:
-            if city is None:
-                city = np.argmin(cost_operations[stage])
-                sub_problem_data.append([city, cost_operations[stage][city]])
+            if self.city is None:
+                self.city = np.argmin(cost_operations[stage])
+                sub_problem_data.append([self.city, cost_operations[stage][self.city]])
             else:
-                cost_total = cost_operations[stage] + trans_cost[city]
-                city = np.argmin(cost_total)
-                sub_problem_data.append([city, cost_total[city]])
+                cost_total = cost_operations[stage] + trans_cost[self.city]
+                self.city = np.argmin(cost_total)
+                sub_problem_data.append([self.city, cost_total[self.city]])
         return np.array(sub_problem_data)[:, 0], np.sum(
             np.array(sub_problem_data)[:, 1]
         )
@@ -43,6 +65,7 @@ class NaiveSolver:
 
         problem_cost = 0
         problem_path = {}
+
         for n_sub in range(n_tasks):
             available_operations = np.nonzero(operation[:, n_sub])[0]
             path, cost = self.solve_suboperaion(
@@ -53,17 +76,5 @@ class NaiveSolver:
             problem_path[f"suboperation_{n_sub}"] = path
         if save_costs:
             self.total_cost += problem_cost
+        self.city = None
         return {"path": problem_path, "cost": problem_cost}
-
-    def solve_all(self):
-        self.total_cost = 0
-        problem_info = {}
-        for num in range(len(self.problems)):
-            result = self.solve_problem(num, save_costs=True)
-
-            problem_info[f"problem_{num}"] = {
-                "path": result["path"],
-                "cost": result["cost"],
-            }
-
-        return problem_info
