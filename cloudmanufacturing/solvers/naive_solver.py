2b3d63ae692232a9af26f15c74aee89387f9bb8b
Dmitrii <dima.fomin.2013@mail.ru>
2023-11-16 00:16:43 +0100

    add gamma to naive solver

--- cloudmanufacturing/solvers/naive_solver.py
+++ cloudmanufacturing/solvers/naive_solver.py
@@ -1,31 +1,43 @@
 import numpy as np
 
 
-class NaiveSolver:
+class naive_solver:
     def __init__(self, dataset):
         self.problems = dataset
         self.total_cost = 0
-        self.city = None
 
-    def solve_suboperaion(self, available_operations, cost_operations, trans_cost):
+        n_tasks = dataset[0]["n_tasks"]
+        n_operations = dataset[0]["n_operations"]
+        n_cities = dataset[0]["n_cities"]
+
+        self.gamma = np.zeros((n_operations, n_tasks, n_cities))
+
+
+    def solve_suboperaion(self, available_operations, cost_operations,
+                        trans_cost, operation_number = None, save_to_gamma = False):
         """
         Solve problem for one suboperation
         """
         sub_problem_data = []
-        city = None
-        for stage in available_operations:
-            if city is None:
+
+        for i, stage in enumerate(available_operations):
+            if i == 0:
                 city = np.argmin(cost_operations[stage])
                 sub_problem_data.append([city, cost_operations[stage][city]])
             else:
                 cost_total = cost_operations[stage] + trans_cost[city]
                 city = np.argmin(cost_total)
-                sub_problem_data.append([city, cost_total[city]])
+                sub_problem_data.append([city, cost_operations[stage][city]])
+
+            if save_to_gamma == True:
+                self.gamma[operation_number, stage, city] = 1
+
         return np.array(sub_problem_data)[:, 0], np.sum(
             np.array(sub_problem_data)[:, 1]
         )
 
-    def solve_problem(self, num_problem, save_costs=False):
+
+    def solve_problem(self, num_problem, save_costs=False, save_to_gamma = False):
         """
         Solve operation
         """
@@ -46,7 +58,8 @@ class NaiveSolver:
         for n_sub in range(n_tasks):
             available_operations = np.nonzero(operation[:, n_sub])[0]
             path, cost = self.solve_suboperaion(
-                available_operations, cost_operations, trans_cost
+                available_operations, cost_operations, trans_cost, operation_number=n_sub,
+                save_to_gamma = save_to_gamma
             )
 
             problem_cost += cost
@@ -55,15 +68,15 @@ class NaiveSolver:
             self.total_cost += problem_cost
         return {"path": problem_path, "cost": problem_cost}
 
+
     def solve_all(self):
         self.total_cost = 0
-        problem_info = {}
-        for num in range(len(self.problems)):
-            result = self.solve_problem(num, save_costs=True)
-
-            problem_info[f"problem_{num}"] = {
-                "path": result["path"],
-                "cost": result["cost"],
+        problem_info = {
+            f"problem_{num}": {
+                "path": self.solve_problem(num, save_costs=True, save_to_gamma=True)["path"],
+                "cost": self.solve_problem(num, save_costs=True, save_to_gamma=True)["cost"],
             }
+            for num in range(len(self.problems))
+        }
 
         return problem_info
